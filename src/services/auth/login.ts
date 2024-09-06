237b77915126a968c34456aadf692f3bfa9df012
BlueOcean <dev.freshworks@gmail.com>
2024-09-06 15:11:34 -0400

    [feat] add service for login and register

---
+++ src/services/auth/login.ts
@@ -0,0 +1,5 @@
+import { AuthProps } from "@/types/auth";
+
+const login = ({ email, password }: AuthProps) => {
+  // TODO: Implement login logic
+};
---
+++ src/services/auth/register.ts
@@ -0,0 +1,5 @@
+import { AuthProps } from "@/types/auth";
+
+const register = ({ email, password }: AuthProps) => {
+  // TODO: Implement register logic
+};
