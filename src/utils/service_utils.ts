3d90c80c4b7cdab7cc12513d598dc61eeee8c1cf
BlueOcean <dev.freshworks@gmail.com>
2024-09-08 20:13:27 -0400

    [update] axios handleError

--- src/utils/service_utils.ts
+++ src/utils/service_utils.ts
@@ -24,18 +24,21 @@ export const runService = async (
 };
 
 export const handleError = (status: number | undefined, error: any) => {
-  console.log(error);
+  let message: string = "";
+  console.log(status);
   switch (status) {
-    case 401:
+    case 403:
       toast.error(error ? error : "Please login to continue!");
       window.location.replace(ROUTE_LOGIN);
       break;
     case 404:
-      const message = error ? error : "Provided data was not found";
+      message = error ? error : "Provided data was not found";
       toast.error(message);
       break;
     case 409:
-      toast.error(error ? error : "Provided data already exists");
+      console.log("error", error);
+      message = error ? error : "Provided data already exists";
+      toast.error(message);
       break;
     case 500:
       toast.error(error ? error : "Internal server error");
