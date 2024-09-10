52817ce227ee961e9f2d14449181add4fa9a3003
doyourbest <creative555.dev@gmail.com>
2024-09-10 16:32:00 +1000

    [refactor] import button

--- src/views/companies.tsx
+++ src/views/companies.tsx
@@ -89,7 +89,7 @@ export default function Companies() {
             </button>
             <div className="px-4 flex flex-1 justify-end items-center">
               <Menu>
-                <MenuButton className="px-2 py-1 flex flex-row items-center gap-1 rounded-md bg-blue-600 hover:bg-blue-500">
+                <MenuButton className="px-2 py-1 flex flex-row items-center gap-1 rounded-md bg-blue-500 hover:bg-blue-400">
                   <span className="text-sm text-white">Import</span>
                   <ChevronDownIcon className="w-3 h-3 stroke-white stroke-2" />
                 </MenuButton>
