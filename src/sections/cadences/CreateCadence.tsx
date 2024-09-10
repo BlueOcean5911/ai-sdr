d1681f43b08fd840dc4c4a6636242af840a1915a
doyourbest <creative555.dev@gmail.com>
2024-09-10 13:00:58 +1000

    [feat] build cadence function

--- src/sections/cadences/CreateCadence.tsx
+++ src/sections/cadences/CreateCadence.tsx
@@ -26,67 +26,67 @@ const CreateCadence = ({
           <div className="flex min-h-full items-center justify-center p-4 ">
             <DialogPanel
               transition
-              className="w-full max-w-lg rounded-xl bg-white p-4 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+              className="w-full max-w-xl rounded-xl bg-white backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
             >
-              <div className="flex justify-end">
+              <div className="px-4 py-2 flex justify-between items-center border-b">
+                <h3>Create New Cadence</h3>
                 <XCircleIcon
-                  className="w-6 g-6 stroke-gray-300 hover:stroke-gray-500 cursor-pointer"
+                  className="w-6 g-6 hover:stroke-gray-600 cursor-pointer"
                   onClick={close}
                 />
               </div>
-              <div className="header border-b-2 border-gray-100">
-                <h3>Create New Cadence</h3>
-                <p className="text-xs text-gray-400 py-2">
+              <div className="px-4 py-2 bg-gray-100">
+                <p className="text-sm">
                   Cadences are a series of automated or manual touchpoints and
                   activities, designed to drive deeper engagement with your
                   contacts.
                 </p>
-              </div>
-              <div className="flex gap-4 p-4">
-                <div
-                  onClick={() => {
-                    if (handleClick) {
-                      handleClick("ai-assisted");
-                    }
-                    close();
-                  }}
-                  className="flex flex-col items-center justify-center text-center flex-1 border-gray-100 border-2 rounded-lg p-2 cursor-pointer hover:shadow-lg"
-                >
-                  <FireIcon className="w-16 h-16 stroke-gray-600 m-6" />
-                  <h3>AI-assisted</h3>
-                  <p className="text-xs text-gray-400">
-                    Create a cadence with AI-assistance
-                  </p>
-                </div>
-                <div
-                  onClick={() => {
-                    if (handleClick) {
-                      handleClick("pre-formatted");
-                    }
-                    close();
-                  }}
-                  className="flex flex-col items-center justify-center text-center flex-1 border-gray-100 border-2 rounded-lg p-2 cursor-pointer hover:shadow-lg"
-                >
-                  <EnvelopeOpenIcon className="w-16 h-16 stroke-gray-600 m-6" />
-                  <h3>Pre-formatted</h3>
-                  <p className="text-xs text-gray-400">
-                    Start with one of pre-formatted cadences
-                  </p>
-                </div>
-                <div
-                  onClick={() => {
-                    if (handleClick) {
-                      handleClick("from-scratch");
-                    }
-                    close();
-                  }}
-                  className="flex flex-col items-center justify-center text-center flex-1 border-gray-100 border-2 rounded-lg p-2 cursor-pointer hover:shadow-lg"
-                >
-                  <PaperAirplaneIcon className="w-16 h-16 stroke-gray-600 m-6" />
-                  <h3>From scratch</h3>
-                  <p className="text-xs text-gray-400">
-                    Create new cadence from scratch
-                  </p>
+                <div className="flex gap-4 p-4">
+                  <div
+                    // onClick={() => {
+                    //   if (handleClick) {
+                    //     handleClick("ai-assisted");
+                    //   }
+                    //   close();
+                    // }}
+                    className="min-w-40 flex flex-col items-center justify-center bg-gray-200 border-2 rounded-lg p-2 cursor-not-allowed hover:shadow-lg"
+                  >
+                    <FireIcon className="w-16 h-16 stroke-gray-600 m-6" />
+                    <h3>AI-assisted</h3>
+                    <p className="text-xs text-gray-400">
+                      Create a cadence with AI-assistance
+                    </p>
+                  </div>
+                  <div
+                    // onClick={() => {
+                    //   if (handleClick) {
+                    //     handleClick("pre-formatted");
+                    //   }
+                    //   close();
+                    // }}
+                    className="min-w-40 flex flex-col items-center justify-center bg-gray-200 border-2 rounded-lg p-2 cursor-not-allowed hover:shadow-lg"
+                  >
+                    <EnvelopeOpenIcon className="w-16 h-16 stroke-gray-600 m-6" />
+                    <h3>Pre-formatted</h3>
+                    <p className="text-xs text-gray-400">
+                      Start with one of pre-formatted cadences
+                    </p>
+                  </div>
+                  <div
+                    onClick={() => {
+                      if (handleClick) {
+                        handleClick("from-scratch");
+                      }
+                      close();
+                    }}
+                    className="min-w-40 flex flex-col items-center justify-center text-center flex-1 bg-white border-2 rounded-lg p-2 cursor-pointer hover:shadow-lg"
+                  >
+                    <PaperAirplaneIcon className="w-16 h-16 stroke-gray-600 m-6" />
+                    <h3>From scratch</h3>
+                    <p className="text-xs text-gray-400">
+                      Create new cadence from scratch
+                    </p>
+                  </div>
                 </div>
               </div>
             </DialogPanel>
