19daa0f88838a20b12b3df50356f6b125a7d4200
doyourbest <creative555.dev@gmail.com>
2024-09-14 18:18:20 +0900

    [fix] issue in build

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,14 @@
 import AuthLayout from "@/layouts/AuthLayout";
+import { Suspense } from "react";
 
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <AuthLayout>{children}</AuthLayout>;
+  return (
+    <AuthLayout>
+      <Suspense>{children}</Suspense>
+    </AuthLayout>
+  );
 }
