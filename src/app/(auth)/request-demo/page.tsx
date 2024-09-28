3047f72fe9fabd22f5be86700b9f1d2edb44d79d
BlueOcean <dev.freshworks@gmail.com>
2024-09-28 10:40:23 -0400

    [update] register page

--- src/app/(auth)/request-demo/page.tsx
+++ src/app/(auth)/request-demo/page.tsx
@@ -311,7 +311,7 @@ export default function Page() {
                         <button
                           type="submit"
                           disabled={isSubmitting}
-                          className="btn-primary flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 disabled:bg-gray-400"
+                          className="btn-primary flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 !disabled:bg-gray-400"
                         >
                           Request a demo
                         </button>
