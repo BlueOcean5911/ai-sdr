0a2f3604cdc8656a40c898dca1826e32b399ee68
BlueOcean <dev.freshworks@gmail.com>
2024-10-03 05:28:32 -0400

    [update] redirect url in next config file

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,6 @@ const nextConfig = {
                 destination: '/dashboard/emails',
                 permanent: true, // Set to true for a permanent redirect (HTTP 308)
             },
-            {
-                source: '/integration/hubspot',
-                destination: '/integration/hubspot/companies',
-                permanent: true, // Set to true for a permanent redirect (HTTP 308)
-            },
         ];
     },
 };
