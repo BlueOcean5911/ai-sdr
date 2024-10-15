87ac653911632dc3d675c58a56fadae2b8a70297
BlueOcean <dev.freshworks@gmail.com>
2024-10-15 12:00:29 -0400

    [update] cadence setting

--- src/app/(dashboard)/cadences/[id]/setting/page.tsx
+++ src/app/(dashboard)/cadences/[id]/setting/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { useEffect, useState } from "react";
+
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
 import {
   BaseCadenceModel,
   getCadenceById,
@@ -10,17 +14,20 @@ import { getUsers } from "@/services/userService";
 import { handleError, runService } from "@/utils/service_utils";
 import Select from "@/components/extends/Select/default";
 import { useCadence } from "@/contexts/CadenceContext";
+import FormHelperText from "@/components/extends/FormHelperText";
 
 export default function Page() {
   const { cadence, setCadence } = useCadence();
   const [userOptions, setUserOptions] = useState<any[]>([]);
 
   const handleUpdateCadence = (updatedCadence: BaseCadenceModel) => {
+    console.log("handleUpdateCadence: ", updatedCadence);
     runService(
       { cadenceId: cadence?.id, updatedCadence },
       updateCadence,
       (data) => {
         setCadence(data);
+        console.log("cadence: ", data);
       },
       (status, error) => {
         console.log(status, error);
@@ -29,25 +36,12 @@ export default function Page() {
     );
   };
 
-  useEffect(() => {
-    runService(
-      cadence.id,
-      getCadenceById,
-      (data) => {
-        setCadence(data);
-      },
-      (status, error) => {
-        handleError(status, error);
-      }
-    );
-  }, []);
-
   const fetchUsers = () => {
     runService(
       undefined,
       getUsers,
-      (users: any[]) => {
-        const tempUserOptions = users.map((user) => {
+      (data: any[]) => {
+        const tempUserOptions = data.map((user) => {
           return {
             name: user.firstName + " " + user.lastName,
             value: user.id,
@@ -60,13 +54,8 @@ export default function Page() {
       }
     );
   };
-
-  const handleSaveCadence = () => {
-    handleUpdateCadence({
-      name: cadence?.name,
-      ownerId: cadence?.ownerId,
-    });
-  };
+  console.log("cadence: ", cadence);
+  console.log("userOptions: ", userOptions);
 
   useEffect(() => {
     fetchUsers();
@@ -77,49 +66,99 @@ export default function Page() {
       <div className="flex flex-1 flex-col overflow-auto">
         <div className="p-4 flex flex-1 bg-gray-100 overflow-auto text-sm">
           <div className="card flex flex-1 justify-center items-center bg-white">
-            <div className="card p-8 max-w-lg min-h-96 w-full flex flex-col gap-5 border shadow-lg">
+            <div className="card py-8 max-w-lg min-h-96 w-full flex flex-col gap-5 border shadow-lg">
               <div className="change-password-header">
                 <h1 className="text-lg">Account Profile</h1>
                 <hr />
               </div>
-              <div className="flex-1 p-2 flex flex-col gap-2 border rounded">
-                <div className="flex flex-col gap-1">
-                  <label className="min-w-24 text-xs" htmlFor="name">
-                    Name:
-                  </label>
-                  <input
-                    id="name"
-                    type="text"
-                    className="input-primary"
-                    value={cadence?.name}
-                    onChange={(e) => {
-                      setCadence({
-                        ...cadence,
-                        name: e.target?.value ? e.target?.value : "",
-                      });
-                    }}
-                  />
-                </div>
-                <div className="flex flex-col gap-1">
-                  <label className="min-w-24 text-xs" htmlFor="owner">
-                    Owner:
-                  </label>
-                  <Select
-                    data={userOptions}
-                    defaultValue={
-                      userOptions.filter(
-                        (user) => user.value === cadence?.ownerId
-                      )[0]
-                    }
-                  ></Select>
-                </div>
-                <div className="flex-1" />
-                <button
-                  className="btn-primary"
-                  onClick={() => handleSaveCadence()}
+              <div className="flex-1 p-4 mt-4 flex flex-col gap-2 border rounded">
+                <Formik
+                  initialValues={{
+                    name: cadence ? cadence.name : "",
+                    ownerId: cadence ? cadence.ownerId : "",
+                  }}
+                  validationSchema={Yup.object().shape({
+                    name: Yup.string().required("Cadence name is required"),
+                    ownerId: Yup.string().required("Assignee is required"),
+                  })}
+                  onSubmit={async (values, { setSubmitting }) => {
+                    setSubmitting(true);
+                    handleUpdateCadence({
+                      name: values.name,
+                      ownerId: values.ownerId,
+                    });
+                    setSubmitting(false);
+                  }}
+                  enableReinitialize={true}
                 >
-                  Save
-                </button>
+                  {({
+                    errors,
+                    handleBlur,
+                    handleChange,
+                    handleSubmit,
+                    setFieldValue,
+                    isSubmitting,
+                    touched,
+                    values,
+                  }) => {
+                    return (
+                      <form
+                        noValidate
+                        onSubmit={handleSubmit}
+                        className="flex-1 flex flex-col gap-2"
+                      >
+                        <div className="flex flex-col gap-1">
+                          <label htmlFor="name" className="text-xs">
+                            Name:
+                          </label>
+                          <input
+                            id="name"
+                            type="text"
+                            placeholder="Name"
+                            className="input-primary max-h-9"
+                            value={values.name}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                          />
+                          {touched.name && errors.name && (
+                            <FormHelperText>{errors.name}</FormHelperText>
+                          )}
+                        </div>
+                        <div className="flex flex-col gap-1">
+                          <label htmlFor="ownerId" className="text-xs">
+                            Owner:
+                          </label>
+                          <Select
+                            data={userOptions}
+                            // defaultValue={userOptions.find(
+                            //   (option) => option.value === values.ownerId
+                            // )}
+                            onChange={(selectedItem) => {
+                              if (
+                                selectedItem &&
+                                selectedItem.value !== values.ownerId
+                              )
+                                setFieldValue("ownerId", selectedItem.value);
+                            }}
+                          ></Select>
+                          {touched.ownerId && errors.ownerId && (
+                            <FormHelperText>{errors.ownerId}</FormHelperText>
+                          )}
+                        </div>
+                        <div className="flex-1" />
+                        <div className="pt-2 flex justify-end gap-4">
+                          <button
+                            type="submit"
+                            disabled={isSubmitting}
+                            className="w-full btn-primary"
+                          >
+                            Save
+                          </button>
+                        </div>
+                      </form>
+                    );
+                  }}
+                </Formik>
               </div>
             </div>
           </div>
