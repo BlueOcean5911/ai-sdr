d805f09642226b70660f2a7f7245be5aefb01a41
doyourbest <creative555.dev@gmail.com>
2024-11-07 03:47:37 -0500

    [update] update create company component
    Co-Authored-By: jamest33 <182724236+jamest33@users.noreply.github.com>

--- src/sections/companies/CreateCompany.tsx
+++ src/sections/companies/CreateCompany.tsx
@@ -1,5 +1,6 @@
-import { statusOptions } from "@/data/filter.data";
-import { CreateCompanyProps } from "@/types";
+import React, { Fragment } from "react";
+import * as Yup from "yup";
+import { Formik } from "formik";
 import {
   Dialog,
   DialogPanel,
@@ -7,19 +8,21 @@ import {
   Transition,
   TransitionChild,
 } from "@headlessui/react";
-import React, { Fragment } from "react";
-import Select from "react-tailwindcss-select";
-import { Formik } from "formik";
-import * as Yup from "yup";
+import { toast } from "react-toastify";
+
+import Select from "@/components/extends/Select/default";
 import FormHelperText from "@/components/extends/FormHelperText";
+
 import {
   addCompany,
   updateCompany,
   BaseCompanyModel,
 } from "@/services/companyService";
 import { runService } from "@/utils/service_utils";
-import { toast } from "react-toastify";
 import { useCompanyFilter } from "@/contexts/FilterCompanyContext";
+
+import { statusOptions_2 } from "@/data/filter.data";
+import { CreateCompanyProps } from "@/types";
 
 const linkedinRegex =
   /^(https?:\/\/)?(www\.)?(linkedin\.com\/(company)\/[a-zA-Z0-9-]+)/;
@@ -58,9 +61,7 @@ export default function CreateCompany({
                     initialValues={{
                       name: company ? company.name : "",
                       phone: company ? company.phone : "",
-                      phoneStatus: statusOptions.find(
-                        (option) => option.value === company?.phoneStatus
-                      ) || statusOptions[3],
+                      phoneStatus: company ? company.phoneStatus : "",
                       companyType: company ? company.companyType : "",
                       description: company ? company.description : "",
                       size: company ? company.size : 0,
@@ -113,13 +114,12 @@ export default function CreateCompany({
                     ) => {
                       console.log("submit");
                       setSubmitting(true);
-                      const phoneStatus = values.phoneStatus?.value;
                       let companyData: BaseCompanyModel = {
                         name: values.name,
                         linkedin: values.linkedin,
                         companyType: values.companyType,
                         phone: values.phone,
-                        phoneStatus: phoneStatus,
+                        phoneStatus: values.phoneStatus,
                         description: values.description,
                         industry: values.industry,
                         streetAddress: values.streetAddress,
@@ -221,40 +221,21 @@ export default function CreateCompany({
                               </div>
                               <div className="w-full flex flex-col">
                                 <Select
-                                  value={
-                                    values.phoneStatus
-                                      ? values.phoneStatus
-                                      : statusOptions[3]
-                                  }
-                                  onChange={(item: any) => {
-                                    setFieldValue("phoneStatus", item);
+                                  data={statusOptions_2}
+                                  defaultValue={statusOptions_2.find(
+                                    (option) =>
+                                      option.value === values.phoneStatus
+                                  )}
+                                  onChange={(selectedItem) => {
+                                    if (
+                                      selectedItem.value !== values.phoneStatus
+                                    )
+                                      setFieldValue(
+                                        "phoneStatus",
+                                        selectedItem.value
+                                      );
                                   }}
-                                  options={statusOptions}
-                                  primaryColor={"indigo"}
-                                  classNames={{
-                                    menuButton: (value) => {
-                                      const isDisabled = value?.isDisabled;
-                                      return `flex text-gray-500 border border-gray-300 rounded shadow-sm transition-all duration-300 focus:outline-none ${
-                                        isDisabled
-                                          ? "bg-gray-200"
-                                          : "bg-white hover:border-gray-400 focus:border-blue-500 focus:ring focus:ring-blue-500/20"
-                                      }`;
-                                    },
-                                    menu: "absolute z-10 w-full bg-white shadow-lg border rounded py-1 mt-1.5 text-gray-700",
-                                    listItem: (value) => {
-                                      const isSelected = value?.isSelected;
-                                      return `block transition duration-200 px-2 py-2 cursor-pointer select-none truncate rounded ${
-                                        isSelected
-                                          ? `text-white bg-blue-500`
-                                          : `text-gray-500 hover:bg-blue-100 hover:text-blue-500`
-                                      }`;
-                                    },
-                                    searchBox:
-                                      "w-full py-2 pl-8 text-sm text-gray-500 bg-gray-100 border border-gray-200 rounded focus:border-gray-200 focus:ring-0 focus:outline-none",
-                                    searchIcon:
-                                      "absolute w-4 h-4 mt-2.5 pb-0.5 ml-1.5 text-gray-500",
-                                  }}
-                                ></Select>
+                                />
                               </div>
                             </div>
                           </div>
@@ -289,9 +270,7 @@ export default function CreateCompany({
                               onBlur={handleBlur}
                             />
                             {touched.industry && errors.industry && (
-                              <FormHelperText>
-                                {errors.industry}
-                              </FormHelperText>
+                              <FormHelperText>{errors.industry}</FormHelperText>
                             )}
                           </div>
 
