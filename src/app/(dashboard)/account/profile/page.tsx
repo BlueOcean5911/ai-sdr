af02b5027ae6d58e0bf2796b5e2a1ae09e17dfa6
BlueOcean <dev.freshworks@gmail.com>
2024-11-08 18:01:27 -0500

    [fix] ui

--- src/app/(dashboard)/account/profile/page.tsx
+++ src/app/(dashboard)/account/profile/page.tsx
@@ -45,7 +45,7 @@ export default function Profile() {
   return (
     <>
       <div className="p-4 flex flex-1 bg-gray-100 overflow-auto text-sm">
-        <div className="card flex flex-1 justify-center items-center bg-white">
+        <div className="flex flex-1 justify-center items-center bg-gray-100">
           <div className="card py-8 max-w-md w-full flex flex-col gap-5 border shadow-lg">
             <div className="change-password-header">
               <h1 className="text-lg">Account Profile</h1>
