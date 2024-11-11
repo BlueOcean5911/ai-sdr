d3e06993f7e83283a6d87be86094edbb687b15ab
doyourbest <creative555.dev@gmail.com>
2024-11-11 08:48:32 -0600

    [feat] add sort to company
    Co-Authored-By: jamest33 <182724236+jamest33@users.noreply.github.com>

--- src/services/companyService.ts
+++ src/services/companyService.ts
@@ -3,7 +3,7 @@ import { CountModel, FetchProps, SuccessModel } from "@/types";
 import { EMAIL_STATUS } from "@/types/enums";
 import {
   CompanyFilterConfig,
-  getDefaultCompanyFilterConfig,
+  defaultCompanyFilterConfig,
 } from "@/contexts/FilterCompanyContext";
 
 interface FetchCompaniesProps extends FetchProps {
@@ -67,68 +67,35 @@ export const getCompanies = async (
     offset: 0,
     limit: 100,
     targeted: false,
-    filter: getDefaultCompanyFilterConfig(),
+    filter: defaultCompanyFilterConfig,
   }
 ): Promise<ApiCompaniesResponse> => {
   const { offset, limit, targeted } = data;
-  const { company, country, state, city, streetAddress, industry } =
-    data?.filter;
-  let url = `/api/companies?offset=${offset}&limit=${limit}`;
+  const {
+    company,
+    industry,
+    country,
+    state,
+    city,
+    streetAddress,
+    orderBy,
+    isAscending,
+  } = data.filter;
+  let url = `/api/companies/?offset=${offset}&limit=${limit}`;
 
-  if (targeted) {
-    url += "&targeted=true";
-  }
-  if (company) {
-    url += `&companyName=${company}`;
-  }
-  if (country) {
-    url += `&country=${country}`;
-  }
-  if (state) {
-    url += `&state=${state}`;
-  }
-  if (city) {
-    url += `&city=${city}`;
-  }
-  if (streetAddress) {
-    url += `&streetAddress=${streetAddress}`;
-  }
-  if (industry) {
-    url += `&industry=${industry}`;
-  }
+  if (targeted) url += `&targeted=true`;
+  if (company) url += `&companyName=${company}`;
+  if (country) url += `&country=${country}`;
+  if (state) url += `&state=${state}`;
+  if (city) url += `&city=${city}`;
+  if (streetAddress) url += `&jobTitle=${streetAddress}`;
+  if (industry) url += `&industry=${industry}`;
+  if (orderBy) url += `&orderBy=${orderBy}`;
+  if (isAscending !== undefined) url += `&isAscending=${isAscending}`;
   const response = await api.get(url);
 
-  let companies: Array<CompanyModel> = [];
-  response.data.forEach((item: any) => {
-    companies.push({
-      id: item?.id,
-      name: item?.name,
-      website: item?.website,
-      linkedin: item?.linkedin,
-      companyType: item?.companyType,
-      phone: item?.phone,
-      phoneStatus: item?.phoneStatus,
-      description: item?.description,
-      industry: item?.industry,
-
-      streetAddress: item?.streetAddress,
-      city: item?.city,
-      state: item?.state,
-      country: item?.country,
-      postalCode: item?.postalCode,
-
-      yearFounded: item?.yearFounded,
-      domain: item?.domain,
-      annualRevenue: item?.annualRevenue,
-      stage: item?.stage,
-      keywords: item?.keywords,
-
-      size: item?.size,
-      targeted: item?.targeted,
-    });
-  });
   return {
-    data: companies,
+    data: response.data,
   };
 };
 
@@ -140,41 +107,15 @@ export const getCompanyById = async (data: {
 
   const response = await api.get(url);
 
-  let company: CompanyModel = {
-    id: response.data?.id,
-    name: response.data?.name,
-    website: response.data?.website,
-    linkedin: response.data?.linkedin,
-    companyType: response.data?.companyType,
-    phone: response.data?.phone,
-    phoneStatus: response.data?.phoneStatus,
-    description: response.data?.description,
-    industry: response.data?.industry,
-
-    streetAddress: response.data?.streetAddress,
-    city: response.data?.city,
-    state: response.data?.state,
-    country: response.data?.country,
-    postalCode: response.data?.postalCode,
-
-    yearFounded: response.data?.yearFounded,
-    domain: response.data?.domain,
-    annualRevenue: response.data?.annualRevenue,
-    stage: response.data?.stage,
-    keywords: response.data?.keywords,
-
-    size: response.data?.size,
-    targeted: response.data?.targeted,
-  };
   return {
-    data: company,
+    data: response.data,
   };
 };
 
 export const getCompanyTotalCount = async (
   data: FetchTotalCountProps = {
     targeted: false,
-    filter: getDefaultCompanyFilterConfig(),
+    filter: defaultCompanyFilterConfig,
   }
 ): Promise<ApiCountResponse> => {
   const { targeted } = data;
@@ -223,32 +164,7 @@ export const addCompany = async (
   }
 
   return {
-    data: {
-      id: response.data?.id,
-      name: response.data?.name,
-      website: response.data?.website,
-      linkedin: response.data?.linkedin,
-      companyType: response.data?.companyType,
-      phone: response.data?.phone,
-      phoneStatus: response.data?.phoneStatus,
-      description: response.data?.description,
-      industry: response.data?.industry,
-
-      streetAddress: response.data?.streetAddress,
-      city: response.data?.city,
-      state: response.data?.state,
-      country: response.data?.country,
-      postalCode: response.data?.postalCode,
-
-      yearFounded: response.data?.yearFounded,
-      domain: response.data?.domain,
-      annualRevenue: response.data?.annualRevenue,
-      stage: response.data?.stage,
-      keywords: response.data?.keywords,
-
-      size: response.data?.size,
-      targeted: response.data?.targeted,
-    },
+    data: response.data,
   };
 };
 
@@ -264,32 +180,7 @@ export const updateCompany = async (data: {
   }
 
   return {
-    data: {
-      id: response.data?.id,
-      name: response.data?.name,
-      website: response.data?.website,
-      linkedin: response.data?.linkedin,
-      companyType: response.data?.companyType,
-      phone: response.data?.phone,
-      phoneStatus: response.data?.phoneStatus,
-      description: response.data?.description,
-      industry: response.data?.industry,
-
-      streetAddress: response.data?.streetAddress,
-      city: response.data?.city,
-      state: response.data?.state,
-      country: response.data?.country,
-      postalCode: response.data?.postalCode,
-
-      yearFounded: response.data?.yearFounded,
-      domain: response.data?.domain,
-      annualRevenue: response.data?.annualRevenue,
-      stage: response.data?.stage,
-      keywords: response.data?.keywords,
-
-      size: response.data?.size,
-      targeted: response.data?.targeted,
-    },
+    data: response.data,
   };
 };
 
