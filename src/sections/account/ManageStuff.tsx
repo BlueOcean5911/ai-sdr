75a2d92774b0688a5e74a7dbbbfffaf710cafa82
doyourbest <creative555.dev@gmail.com>
2024-11-10 19:03:20 -0600

    [update] update user table page
    Co-Authored-By: jamest33 <182724236+jamest33@users.noreply.github.com>

--- src/sections/account/ManageStuff.tsx
+++ src/sections/account/ManageStuff.tsx
@@ -4,6 +4,7 @@ import { PlusCircleIcon, TrashIcon } from "@heroicons/react/24/outline";
 import { toast } from "react-toastify";
 
 import InviteUser from "./InviteUser";
+import DeleteUser from "./DeleteUser";
 import ToggleButton from "@/components/extends/Button/ToggleButton";
 import Loading from "@/components/Loading";
 
@@ -21,6 +22,8 @@ const ManageStuff = () => {
   const [me, setMe] = useState<UserModel>();
   const [users, setUsers] = useState<UserModel[]>();
   const [invite, setInvite] = useState(false);
+  const [curUser, setCurUser] = useState<UserModel | undefined>(undefined);
+  const [deleteUserModal, setDeleteUserModal] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const fetchUsers = () => {
@@ -43,7 +46,7 @@ const ManageStuff = () => {
       undefined,
       getMe,
       (data) => {
-        console.log("users: ", data);
+        console.log("me: ", data);
         setMe(data);
       },
       (status, error) => {
@@ -70,7 +73,13 @@ const ManageStuff = () => {
     );
   };
 
+  const handleConfirmDelete = (user: UserModel) => {
+    setCurUser(user);
+    setDeleteUserModal(true);
+  };
+
   const handleDelete = (userId: string) => {
+    setDeleteUserModal(false);
     runService(
       userId,
       deleteUser,
@@ -93,27 +102,31 @@ const ManageStuff = () => {
   }, []);
   return (
     <>
-      {invite && (
-        <InviteUser
-          open={invite}
-          handleInvite={(email: string) => {
-            runService(
-              { email },
-              sendInviteLink,
-              (data) => {
-                if (data.success === true) {
-                  toast.success("Successfully sent invite.");
-                } else toast.error("Something goes wrong.");
-              },
-              (status, error) => {
-                handleError(status, error);
-              }
-            );
-            setInvite(false);
-          }}
-          handleClose={() => setInvite(false)}
-        />
-      )}
+      <InviteUser
+        open={invite}
+        handleInvite={(email: string) => {
+          runService(
+            { email },
+            sendInviteLink,
+            (data) => {
+              if (data.success === true) {
+                toast.success("Successfully sent invite.");
+              } else toast.error("Something goes wrong.");
+            },
+            (status, error) => {
+              handleError(status, error);
+            }
+          );
+          setInvite(false);
+        }}
+        handleClose={() => setInvite(false)}
+      />
+      <DeleteUser
+        open={deleteUserModal}
+        user={curUser}
+        handleDelete={(userId) => handleDelete(userId)}
+        handleClose={() => setDeleteUserModal(false)}
+      />
       <div className="card flex flex-1 flex-col gap-2 bg-white overflow-auto">
         <div className="flex justify-between items-center">
           <div />
@@ -131,97 +144,100 @@ const ManageStuff = () => {
 
         {/* Table */}
         <div className="flex-1 overflow-auto border rounded">
-          {loading?(<Loading />):(<table className="w-full divide-y divide-gray-300">
-            <thead className="bg-white sticky top-0 z-10">
-              <tr>
-                <th
-                  scope="col"
-                  className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3"
-                >
-                  First Name
-                </th>
-                <th
-                  scope="col"
-                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                >
-                  Last Name
-                </th>
-                <th
-                  scope="col"
-                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                >
-                  Email
-                </th>
-                <th
-                  scope="col"
-                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                >
-                  Title
-                </th>
-                <th
-                  scope="col"
-                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                >
-                  Phone
-                </th>
-                <th
-                  scope="col"
-                  className="py-3.5 text-left text-sm font-semibold text-gray-900"
-                ></th>
-              </tr>
-            </thead>
-            <tbody className="bg-white overflow-auto">
-              {users &&
-                users.map((user, index) => (
-                  <tr key={index} className="even:bg-blue-50 hover:bg-gray-300">
-                    <td className="whitespace-nowrap py-3 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
-                      {user.firstName}
-                    </td>
-                    <td className="whitespace-nowrap py-3 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
-                      {user.lastName}
-                    </td>
-                    <td className="whitespace-nowrap py-3 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
-                      {user.email}
-                    </td>
-                    <td className="whitespace-nowrap py-3 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
-                      {user.title}
-                    </td>
-                    <td className="whitespace-nowraptext-sm text-gray-500">
-                      {user.phone}
-                    </td>
-                    <td className="py-2 flex flex-1 justify-center items-center gap-4">
-                      {me && user.id !== me.id ? (
-                        <>
-                          <ToggleButton
-                            checked={user.enabled ? user.enabled : false}
-                            handleChange={() => {
-                              handleUpdate({
-                                id: user.id,
-                                enabled: !user.enabled,
-                              });
-                            }}
-                          />
-                          <div
-                            className="p-1 rounded-md cursor-pointer hover:bg-gray-100"
-                            onClick={() =>
-                              handleDelete(user.id ? user.id : "fakeUserId")
-                            }
-                          >
-                            <TrashIcon className="w-5 h-5 stroke-red-500" />
+          {loading ? (
+            <Loading />
+          ) : (
+            <table className="w-full divide-y divide-gray-300">
+              <thead className="bg-white sticky top-0 z-10">
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3"
+                  >
+                    First Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Last Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Email
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Title
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Phone
+                  </th>
+                  <th
+                    scope="col"
+                    className="py-3.5 text-left text-sm font-semibold text-gray-900"
+                  ></th>
+                </tr>
+              </thead>
+              <tbody className="bg-white overflow-auto">
+                {users &&
+                  users.map((user, index) => (
+                    <tr
+                      key={index}
+                      className="even:bg-blue-50 hover:bg-gray-300"
+                    >
+                      <td className="whitespace-nowrap py-3 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
+                        {user.firstName}
+                      </td>
+                      <td className="whitespace-nowrap py-3 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
+                        {user.lastName}
+                      </td>
+                      <td className="whitespace-nowrap py-3 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
+                        {user.email}
+                      </td>
+                      <td className="whitespace-nowrap py-3 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
+                        {user.title}
+                      </td>
+                      <td className="whitespace-nowraptext-sm text-gray-500">
+                        {user.phone}
+                      </td>
+                      <td className="py-2 flex flex-1 justify-center items-center gap-4">
+                        {me && user.id !== me.id ? (
+                          <>
+                            <ToggleButton
+                              checked={user.enabled ? user.enabled : false}
+                              handleChange={() => {
+                                handleUpdate({
+                                  id: user.id,
+                                  enabled: !user.enabled,
+                                });
+                              }}
+                            />
+                            <div
+                              className="p-1 rounded-md cursor-pointer hover:bg-gray-100"
+                              onClick={() => handleConfirmDelete(user)}
+                            >
+                              <TrashIcon className="w-5 h-5 stroke-red-500" />
+                            </div>
+                          </>
+                        ) : (
+                          <div className="p-1 rounded-md cursor-pointer hover:bg-gray-100">
+                            Current User
                           </div>
-                        </>
-                      ) : (
-                        <div
-                          className="p-1 rounded-md cursor-pointer hover:bg-gray-100"
-                        >
-                          Current User
-                        </div>
-                      )}
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>)}
+                        )}
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          )}
         </div>
       </div>
     </>
