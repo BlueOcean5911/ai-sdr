b0a68253c0d63272f89e55ca5a97384b993a6ad4
doyourbest <creative555.dev@gmail.com>
2024-11-11 12:29:56 -0600

    [update] update email item to table
    Co-Authored-By: jamest33 <182724236+jamest33@users.noreply.github.com>

--- src/sections/emails/EmailItem.tsx
+++ src/sections/emails/EmailItem.tsx
@@ -1,5 +1,4 @@
 import React, { Fragment, useState } from "react";
-import { EmailItemProps } from "@/types";
 import {
   Menu,
   MenuButton,
@@ -11,36 +10,26 @@ import {
   Transition,
   TransitionChild,
 } from "@headlessui/react";
-import {
-  StarIcon,
-  EllipsisHorizontalIcon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";
-import { useRouter } from "next/navigation";
-import EmailSendWindow from "@/sections/email/EmailSendWindow";
+import { EllipsisHorizontalIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { MailingModel } from "@/services/mailingService";
 import { formatDate, formatDateTimeReadable } from "@/utils/format";
 
 export default function EmailItem({ mailing }: { mailing: MailingModel }) {
   const [sent, setSent] = useState(false);
 
-  const router = useRouter();
-
   return (
-    <div className="w-full h-28 py-2 flex items-center border-b even:bg-blue-50 hover:bg-gray-100">
-      <div className="px-4">
+    <tr
+      key={mailing.id}
+      className="h-24 even:bg-blue-50 hover:bg-gray-300 cursor-pointer"
+    >
+      <td className="px-4">
         <input className="shadow-none ring-0 focus:ring-0" type="checkbox" />
-      </div>
-      <div
-        className="flex items-center flex-1 gap-2 cursor-pointer"
-        onClick={() => setSent(true)}
-      >
-        <div className="min-w-28 flex items-center overflow-hidden max-w-28">
-          <span className="text-xs">To:</span>
-          <span className="text-sm text-blue-900">{mailing.leadName}</span>
-        </div>
-
-        <div className="flex flex-1 flex-col gap-0.5 text-xs">
+      </td>
+      <td onClick={() => setSent(true)}>
+        <span className="text-sm text-blue-900">{mailing.leadName}</span>
+      </td>
+      <td>
+        <div className="flex flex-col gap-0.5 text-xs">
           <span className="font-semibold line-clamp-1 text-sm">
             {mailing.subject}
           </span>
@@ -49,19 +38,21 @@ export default function EmailItem({ mailing }: { mailing: MailingModel }) {
             Step {mailing.currentCadenceStep} of {mailing.cadenceName}
           </span>
         </div>
+      </td>
 
-        <div className="flex  gap-2 text-xs flex-col">
-          <p className="p-1 rounded-full bg-gray-100 text-center">
-            {mailing.ownerName}
-          </p>
-          <div className="flex items-center gap-2">
-            <span className="px-1 flex flex-1 justify-center bg-blue-500 text-white capitalize">
-              {mailing.mailingStatus}
-            </span>
-            <span>{formatDate(mailing.scheduledAt)}</span>
-          </div>
-        </div>
-      </div>
+      <td>
+        <span className="p-1 text-xs rounded-full bg-gray-100 text-center">
+          {mailing.ownerName}
+        </span>
+      </td>
+      <td>
+        <span className="p-1 text-xs text-center rounded-full bg-blue-500 text-white capitalize">
+          {mailing.mailingStatus}
+        </span>
+      </td>
+      <td>
+        <span className="text-xs">{formatDate(mailing.scheduledAt)}</span>
+      </td>
       <Transition appear show={sent} as={Fragment}>
         <Dialog as="div" className="relative" onClose={() => setSent(false)}>
           <div className="fixed inset-0 bg-black/65 z-40" />
@@ -130,10 +121,10 @@ export default function EmailItem({ mailing }: { mailing: MailingModel }) {
         </Dialog>
       </Transition>
 
-      <div className="px-4 flex items-center">
+      <td className="px-4">
         <Menu>
-          <MenuButton className="">
-            <div className="p-1 border rounded-md">
+          <MenuButton>
+            <div className="p-1 border rounded-md hover:bg-white">
               <EllipsisHorizontalIcon className="w-5 h-5 stroke-gray-500" />
             </div>
           </MenuButton>
@@ -148,7 +139,7 @@ export default function EmailItem({ mailing }: { mailing: MailingModel }) {
             </MenuItem>
             <MenuItem>
               <button className="p-2 text-xs flex w-full items-center rounded-lg data-[focus]:bg-blue-100">
-                Skip Email and Continue Cadence
+                Skispan Email and Continue Cadence
               </button>
             </MenuItem>
             <MenuItem>
@@ -158,7 +149,7 @@ export default function EmailItem({ mailing }: { mailing: MailingModel }) {
             </MenuItem>
           </MenuItems>
         </Menu>
-      </div>
-    </div>
+      </td>
+    </tr>
   );
 }
