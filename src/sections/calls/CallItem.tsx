8dbb88c368d35ab4657a344c2f472dd0d8ec5801
doyourbest <creative555.dev@gmail.com>
2024-12-04 23:36:46 -0600

    [update] update call item

--- src/sections/calls/CallItem.tsx
+++ src/sections/calls/CallItem.tsx
@@ -1,8 +1,11 @@
+"use client";
 import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/react";
 import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
 import { CallModel } from "@/services/callService";
 import { SlCallIn } from "react-icons/sl";
 import { SlCallOut } from "react-icons/sl";
+
+import { formatTimestamp } from "@/utils/format";
 
 export default function CallItem({
   call,
@@ -14,53 +17,51 @@ export default function CallItem({
   handleDelete: () => void;
 }) {
   return (
-    <div className="w-full h-20 px-4 py-2 flex items-center gap-2 border-b hover:bg-gray-100">
-      <input className="shadow-none ring-0 focus:ring-0" type="checkbox" />
-      <div className="px-3 py-1 cursor-pointer rounded-md hover:bg-gray-100">
-        {call.direction ? (
+    <tr className="group h-20 even:bg-blue-50 hover:bg-gray-100 hover:cursor-pointer border-b">
+      <td className="px-4 py-2 w-10">
+        <input
+          className="mr-1 shadow-none ring-0 focus:ring-0"
+          type="checkbox"
+        />
+      </td>
+      <td className="px-3 py-1" >
+        {call.direction === "inbound" ? (
           <SlCallIn className="w-5 h-5 fill-blue-900 stroke-blue-900" />
         ) : (
           <SlCallOut className="w-5 h-5 fill-blue-900 stroke-blue-900" />
         )}
-      </div>
-      <div className="flex items-center flex-1 gap-4 cursor-pointer">
-        <div className="min-w-32 flex flex-col gap-1">
-          <span className="text-sm font-semibold text-blue-900 line-clamp-1">
-            {call.user}
-          </span>
-          <span className="text-xs line-clamp-1">{call.userPhone}</span>
-        </div>
-
-        <div className="min-w-32 flex flex-col gap-1">
-          <span className="text-sm font-semibold text-blue-900 line-clamp-1">
-            {call.contact}
-          </span>
-          <span className="text-xs line-clamp-1">{call.contactPhone}</span>
-        </div>
-
-        <div className="min-w-20 flex flex-col gap-1">
-          <span className="text-sm">{call.time}</span>
-          <span className="text-xs">{call.date}</span>
-        </div>
-
-        <span className="min-w-16 p-1 text-xs font-semibold text-nowrap">
-          {call.duration}
+      </td>
+      <td className="min-w-32">
+        <span className="text-sm font-semibold text-blue-900 line-clamp-1">
+          {call.from_formatted || "Unknown"}
         </span>
-
-        <span className="min-w-32 p-1 text-xs font-semibold text-nowrap">
-          {call.purpose}
-        </span>
-
-        <span className="min-w-20 p-1 text-xs font-semibold text-nowrap">
-          {call.disposition}
-        </span>
-
-        <span className="min-w-32 p-1 text-xs font-semibold text-nowrap">
-          {call.note}
-        </span>
-      </div>
-
-      <div className="flex items-center">
+        <span className="text-xs line-clamp-1">{call.from_ || "Unknown"}</span>
+      </td>
+      <td className="min-w-32">
+        <p className="text-sm font-semibold text-blue-900 line-clamp-1">
+          {call.to || "Unknown"}
+        </p>
+        <p className="text-xs line-clamp-1">
+          {call.to_formatted || "Unknown"}
+        </p>
+      </td>
+      <td className="min-w-20">
+        <p className="text-sm">{call.start_time.split("T")[0]}</p>
+        <span className="text-xs">{call.start_time.split("T")[1].replace("Z", "")}</span>
+      </td>
+      <td className="min-w-16 p-1 text-xs text-nowrap">
+        {formatTimestamp(call.duration)}
+      </td>
+      <td className="min-w-32 p-1 text-xs font-semibold text-nowrap capitalize">
+        {call.status}
+      </td>
+      <td className="min-w-20 p-1 text-xs font-semibold text-nowrap">
+        {call.price}
+      </td>
+      <td className="min-w-32 p-1 text-xs font-semibold text-nowrap">
+        {call.price_unit}
+      </td>
+      <td className="">
         <Menu>
           <MenuButton className="">
             <div className="p-1 border rounded-md">
@@ -94,7 +95,8 @@ export default function CallItem({
             </MenuItem>
           </MenuItems>
         </Menu>
-      </div>
-    </div>
+      </td>
+    </tr>
   );
 }
+
