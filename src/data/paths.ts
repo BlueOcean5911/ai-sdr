9e77184a255e3274c5db574474d99e927de8d607
doyourbest <creative555.dev@gmail.com>
2024-12-07 20:23:16 -0600

    [feat] add public and private paths

---
+++ src/data/paths.ts
@@ -0,0 +1,16 @@
+export const publicPaths = ["/login", "/request-demo"];
+
+export const privatePaths = [
+  "/dashboard",
+  "/leads",
+  "/companies",
+  "/personas",
+  "/integration",
+  "/campaingns",
+  "/cadences",
+  "/emails",
+  "/calls",
+  "/tasks",
+  "/account",
+  "/settings",
+];
