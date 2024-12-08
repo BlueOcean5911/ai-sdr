cca04ca2355cfe6f171256231870686d1f7d5ee0
doyourbest <creative555.dev@gmail.com>
2024-12-07 20:23:47 -0600

    [feat] create jwt function

---
+++ src/utils/jwt.ts
@@ -0,0 +1,9 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+export const isTokenExpired = (token: string): boolean => {
+  const decoded = jwt.decode(token) as JwtPayload;
+  if (decoded && decoded.exp) {
+    return decoded.exp * 1000 < Date.now(); // Convert exp to milliseconds
+  }
+  return true; // If no exp, consider it expired
+};
