c7ab8f6f4a52cf5e8597bd6b5f817feb1b362776
doyourbest <creative555.dev@gmail.com>
2024-12-10 06:12:10 -0600

    [update] plan card

--- src/data/plan.data.ts
+++ src/data/plan.data.ts
@@ -1,25 +1,57 @@
 export const PLANS = {
   FREE: {
+    id: "Free",
     name: "Free",
-    price: 0,
+    monthly: {
+      priceId: "price_1QUOPRFDDG0Fe3Xz6TygzvNh",
+      value: 0,
+    },
+    annually: {
+      priceId: "price_1QUOPRFDDG0Fe3Xz6TygzvNh",
+      value: 0,
+    },
     exportCredits: "10 Export Credits/Month",
     mobileCredits: "5 Mobile Number Credits/Year",
   },
   BASIC: {
+    id: "Basic",
     name: "Basic",
-    price: 49,
+    monthly: {
+      priceId: "price_1QUOIsFDDG0Fe3XzRBuJUim3",
+      value: 59,
+    },
+    annually: {
+      priceId: "price_1QUOItFDDG0Fe3XzHg0dI1jN",
+      value: 49,
+    },
     exportCredits: "1,000 Export Credits/Month",
     mobileCredits: "75 Mobile Number Credits/Month",
   },
   PROFESSIONAL: {
+    id: "Professional",
     name: "Professional",
-    price: 79,
+    monthly: {
+      priceId: "price_1QUOKWFDDG0Fe3XzgkSulUjd",
+      value: 99,
+    },
+    annually: {
+      priceId: "price_1QUOKWFDDG0Fe3Xz2gKYAvSH",
+      value: 79,
+    },
     exportCredits: "2000 Export Credits/Year",
     mobileCredits: "100 Mobile Number Credits/Year",
   },
   ORGANIZATION: {
+    id: "Organization",
     name: "Organization",
-    price: 119,
+    monthly: {
+      priceId: "price_1QUOMfFDDG0Fe3Xz9q7sCpmh",
+      value: 149,
+    },
+    annually: {
+      priceId: "price_1QUOMfFDDG0Fe3XzjgLpeCi4",
+      value: 119,
+    },
     exportCredits: "4000 Export Credits/Year",
     mobileCredits: "200 Mobile Number Credits/Year",
   },
