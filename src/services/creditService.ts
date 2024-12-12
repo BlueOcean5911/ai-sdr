060a6d3b32feb845a804539d222528c751bffdec
doyourbest <creative555.dev@gmail.com>
2024-12-12 02:49:13 -0600

    [update] credit service

--- src/services/creditService.ts
+++ src/services/creditService.ts
@@ -1,5 +1,29 @@
 import { api } from "@/utils/api";
 
+export interface UpdateCreditModel {
+  emailReg: number;
+  emailExt: number;
+  mobileReg: number;
+  mobileExt: number;
+}
+
 export const getMyPlan = () => {
   return api.get("/api/credits/my-plan");
+};
+
+export const updateCredit = async (data: {
+  creditId: string;
+  updateData: UpdateCreditModel;
+}) => {
+  const { creditId, updateData } = data;
+  // console.log("send credit", updateData);
+  const response = await api.put(`api/credits/${creditId}`, updateData);
+  // console.log("send credit", response.data);
+  if (response.status !== 200) {
+    throw new Error("Failed to update credit");
+  }
+
+  return {
+    data: response.data,
+  };
 };
