7ff9cdb2012cf65d2df38cba2155ab6d049d314c
doyourbest <creative555.dev@gmail.com>
2024-12-13 00:29:09 -0600

    [update] contact table

--- src/views/generalContacts.tsx
+++ src/views/generalContacts.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import SortableHeader from "@/components/ui/SortableHeader";
 import ContactItem from "@/sections/contacts/ContactItem";
 import { ContactInCadence } from "@/services/contactsService";
 import { LEAD_STAGE } from "@/types/enums";
@@ -20,25 +21,90 @@ export default function GeneralContacts({
   return (
     <>
       {/* Table */}
-      <div className="flex flex-1 flex-col w-full align-middle overflow-auto">
-        <div className="w-full h-full border rounded-md overflow-auto">
-          {contacts.length > 0 ? (
-            contacts.map((contact, index) => (
-              <ContactItem
-                key={index}
-                contact={contact}
-                pause={pause}
-                resume={resume}
-                remove={remove}
-                finish={finish}
-              />
-            ))
-          ) : (
-            <div className="w-full h-full flex-center">
-              <p className="text-gray-900 text-sm">No contacts found.</p>
-            </div>
-          )}
-        </div>
+      <div className="flex flex-1 flex-col w-full align-middle border rounded-md overflow-auto">
+        <table className="flex-1 w-full">
+          <thead className="sticky top-0 z-10 bg-gray-50 shadow-md">
+            <tr>
+              <th></th>
+              <th>
+                <SortableHeader
+                  label="to"
+                  value="To"
+                  orderBy={""}
+                  isAscending={true}
+                  handleChangeSort={() => {}}
+                />
+              </th>
+              <th>
+                <SortableHeader
+                  label="status"
+                  value="status"
+                  orderBy={""}
+                  isAscending={true}
+                  handleChangeSort={() => {}}
+                />
+              </th>
+              <th>
+                <SortableHeader
+                  label="step"
+                  value="step"
+                  orderBy={""}
+                  isAscending={true}
+                  handleChangeSort={() => {}}
+                />
+              </th>
+              <th>
+                <SortableHeader
+                  label="title & company"
+                  value="titleAndCompany"
+                  orderBy={""}
+                  isAscending={true}
+                  handleChangeSort={() => {}}
+                />
+              </th>
+              <th>
+                <SortableHeader
+                  label="created At"
+                  value="createdAt"
+                  orderBy={""}
+                  isAscending={true}
+                  handleChangeSort={() => {}}
+                />
+              </th>
+              <th>
+                <SortableHeader
+                  label="owner"
+                  value="ownerId"
+                  orderBy={""}
+                  isAscending={true}
+                  handleChangeSort={() => {}}
+                />
+              </th>
+              <th className="py-2 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {contacts.length > 0 ? (
+              contacts.map((contact, index) => (
+                <ContactItem
+                  key={index}
+                  contact={contact}
+                  pause={pause}
+                  resume={resume}
+                  remove={remove}
+                  finish={finish}
+                />
+              ))
+            ) : (
+              <div className="w-full h-full flex-center">
+                <p className="text-gray-900 text-sm">No contacts found.</p>
+              </div>
+            )}
+            <tr></tr>
+          </tbody>
+        </table>
       </div>
     </>
   );
