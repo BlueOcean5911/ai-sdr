85a2c95d7d532c017e827f4917decb59c22b272c
doyourbest <creative555.dev@gmail.com>
2024-12-13 12:36:40 -0600

    [update] contact toolbar

--- src/sections/contacts/ContactToolbar.tsx
+++ src/sections/contacts/ContactToolbar.tsx
@@ -1,4 +1,5 @@
 import { useContactFilter } from "@/contexts/FilterContactContext";
+import { classNames } from "@/utils";
 import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/outline";
 
 const stageDict: Record<string, string> = {
@@ -38,10 +39,26 @@ const ContactToolbar = ({ data }: { data: any }) => {
       </button>
       <div className="flex gap-2 overflow-auto">
         <div className="flex-1 overflow-auto flex gap-2">
-          {Object.keys(stageDict).map((stage: any) => (
+          {Object.keys(stageDict).map((stage: string) => (
             <div
               key={stage}
-              className="min-w-20 max-w-28 py-1 flex flex-col text-xs text-center cursor-pointer border-b hover:bg-gray-100 hover:border-b-blue-600"
+              className={classNames(
+                "min-w-20 max-w-28 py-1 flex flex-col text-xs text-center cursor-pointer border-b hover:bg-gray-100 hover:border-b-blue-600",
+                (stage === "totalCount" &&
+                  contactFilterConfig.stage === undefined) ||
+                  stage.replace("Count", "") === contactFilterConfig.stage
+                  ? "border-b-blue-500 bg-gray-100"
+                  : "hover:bg-gray-100 hover:border-b-blue-500"
+              )}
+              onClick={() =>
+                setContactFilterConfig({
+                  ...contactFilterConfig,
+                  stage:
+                    stage === "totalCount"
+                      ? undefined
+                      : stage.replace("Count", ""),
+                })
+              }
             >
               <span className="text-inherit">{data ? data[stage] : "-"}</span>
               <span className="text-inherit text-nowrap overflow-hidden">
