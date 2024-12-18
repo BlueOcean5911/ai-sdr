5ee2598896ac244c70a7af6727e54a3d27d4de29
doyourbest <creative555.dev@gmail.com>
2024-12-18 03:05:52 -0600

    [update] header alert

--- src/components/DashboardLayout/Alert/header-alert.tsx
+++ src/components/DashboardLayout/Alert/header-alert.tsx
@@ -10,8 +10,7 @@ import Link from "next/link";
 import { ROUTE_ALERTS } from "@/data/routes";
 
 export default function HeaderAlert() {
-  const { alerts, handleMarkAsRead } = useAlert();
-  const filteredAlerts = alerts.filter((alert) => !alert.isRead);
+  const { unreadAlerts, handleMarkAsRead } = useAlert();
 
   const router = useRouter();
 
@@ -22,18 +21,18 @@ export default function HeaderAlert() {
           <span className="sr-only">Open Alert</span>
           <div className="relative">
             <BellAlertIcon className="h-6 w-6" aria-hidden="true" />
-            {filteredAlerts.length > 0 && (
+            {unreadAlerts.length > 0 && (
               <span className="absolute -top-1 1right-1 inline-flex items-center justify-center w-4 h-4 text-xs font-medium text-white bg-red-600 rounded-full">
-                {filteredAlerts.length}
+                {unreadAlerts.length}
               </span>
             )}
           </div>
         </MenuButton>
         <MenuItems
           transition
-          className="flex flex-col absolute right-0 z-50 mt-2.5 min-w-96 max-w-96 max-h-[600px] origin-top-right rounded-md bg-white shadow-lg ring-1 ring-gray-900/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+          className="flex flex-col absolute right-0 z-50 mt-2.5 min-w-96 max-w-96 max-h-96 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-gray-900/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
         >
-          <div className="px-6 py-4 border-b border-gray-100 flex items-center justify-between">
+          <div className="px-6 py-4 border-b border-gray-100 flex items-center justify-between shadow-sm">
             <div className="flex items-center gap-2">
               <Bell className="w-5 h-5 text-gray-500" />
               <h2 className="text-lg font-semibold text-gray-900">Alerts</h2>
@@ -41,8 +40,8 @@ export default function HeaderAlert() {
           </div>
           <div className="flex flex-col flex-1 overflow-y-auto">
             <div className="flex-1 overflow-y-auto">
-              {alerts.length > 0 ? (
-                filteredAlerts.map((item, id) => (
+              {unreadAlerts.length > 0 ? (
+                unreadAlerts.map((item, id) => (
                   <MenuItem key={id}>
                     <div
                       className="block px-3 py-2 text-sm leading-6 border-b text-gray-900 data-[focus]:bg-blue-50 data-[focus]:cursor-pointer"
@@ -98,11 +97,12 @@ export default function HeaderAlert() {
                 </>
               )}
             </div>
-            <div className="px-6 py-4 bg-gray-50 flex justify-end gap-3">
-              <Link href={ROUTE_ALERTS}>
-                <button className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors duration-150 rounded-lg">
-                  View All
-                </button>
+            <div className="px-8 py-4 bg-gray-50 flex justify-end gap-3 border-t">
+              <Link
+                href={ROUTE_ALERTS}
+                className="text-sm font-medium text-blue-600 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors duration-150 rounded-lg"
+              >
+                View All
               </Link>
             </div>
           </div>
