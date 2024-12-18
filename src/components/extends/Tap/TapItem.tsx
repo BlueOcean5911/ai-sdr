c8a60ca4e8df3e0631b1669697fd2dbe1468476f
blueocean <dev.freshworks@gmail.com>
2024-12-17 22:26:29 -0500

    [update] TapItem for UI

--- src/components/extends/Tap/TapItem.tsx
+++ src/components/extends/Tap/TapItem.tsx
@@ -18,10 +18,10 @@ const TapItem = ({
       <Link href={link}>
         <button
           className={`
-                group inline-flex items-center px-1 py-1   text-sm font-medium border-b-2 hover:border-gray-300 
+                group inline-flex items-center px-1 py-1 text-sm font-medium border-b-2 hover:border-gray-300 
                 ${
                   active
-                    ? "border-blue-400 text-blue-400"
+                    ? "border-blue-400"
                     : "border-transparent text-gray-400 hover:text-gray-700"
                 }
               `}
@@ -31,7 +31,7 @@ const TapItem = ({
                 inline-flex items-center gap-2 transition-colors duration-200
                 ${
                   active
-                    ? "text-blue-400 stroke-blue-400"
+                    ? "text-gray-00 stroke-gray-900"
                     : "text-gray-400 group-hover:text-gray-400"
                 }
               `}
@@ -39,7 +39,7 @@ const TapItem = ({
             {React.cloneElement(icon as React.ReactElement, {
               className: `w-4 h-4 ${
                 active
-                  ? "fill-blue-400 stroke-blue-400"
+                  ? "fill-gray-900 stroke-gray-900"
                   : "text-gray-400 group-hover:text-gray-400"
               }`,
             })}
